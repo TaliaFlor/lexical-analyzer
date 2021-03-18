30a2b3856e38189df24332caa351cbf780e3bcb6
TaliaFlor <talianadegia@gmail.com>
2021-03-18 00:21:12 -0300

    Comenta fora os testes unitários

--- src/test/java/compiler/ReadCodeTest.java
+++ src/test/java/compiler/ReadCodeTest.java
@@ -8,22 +8,22 @@ import org.junit.jupiter.api.Test;
 
 public class ReadCodeTest {
 
-    private static LexicalScanner lexicalScanner;
-
-    @BeforeAll
-    static void initAll() {
-        lexicalScanner = new LexicalScanner("somatorio.c");
-    }
-
-    @Test
-    void testCodeCompiles() {
-        Assertions.assertDoesNotThrow(() -> {
-            Token token;
-            do {
-                token = lexicalScanner.nextToken();
-                System.out.println(token);
-            } while (token != null);
-        });
-    }
+//    private static LexicalScanner lexicalScanner;
+//
+//    @BeforeAll
+//    static void initAll() {
+//        lexicalScanner = new LexicalScanner("somatorio.c");
+//    }
+//
+//    @Test
+//    void testCodeCompiles() {
+//        Assertions.assertDoesNotThrow(() -> {
+//            Token token;
+//            do {
+//                token = lexicalScanner.nextToken();
+//                System.out.println(token);
+//            } while (token != null);
+//        });
+//    }
 
 }
