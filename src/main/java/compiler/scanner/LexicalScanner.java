8edef56e5ca8fdb550901c35167a165cab4204f5
TaliaFlor <talianadegia@gmail.com>
2021-04-15 19:50:49 -0300

    Separa estados em métodos

--- src/main/java/compiler/scanner/LexicalScanner.java
+++ src/main/java/compiler/scanner/LexicalScanner.java
@@ -100,186 +100,64 @@ public class LexicalScanner {   //TODO adicionar linha (/r) e coluna (cada carac
         TokenType type;
         switch (state) {
             case ZERO:
-                if (isNonConsumable(c))
-                    break;
-
-                append(c);
-                if (isNumber(c))
-                    state = State.ONE;
-                else if (isUnderline(c) || isLetter(c))
-                    state = State.SIX;
-                else if (isEquals(c))
-                    state = State.EIGHT;
-                else if (isAritmeticOperator(c))
-                    state = State.ELEVEN;
-                else if (isSpecialChar(c)) {
-                    back();
-                    state = State.TWELVE;
-                } else if (isRelationalOperator(c))
-                    state = State.THIRTEEN;
-                else if (isSingleQuotes(c))
-                    state = State.TWENTY_TWO;
-                else
-                    throw new UnrecognizedTokenException("Unrecognized symbol - '" + (scanned + c) + "'");
+                stateZero(c);
                 break;
             case ONE:
-                if (isNumber(c)) {
-                    append(c);
-                    break;
-                } else if (isDot(c)) {
-                    append(c);
-                    state = State.THREE;
-                } else if (isNonConsumable(c) || !isLetter(c)) {
-                    back();
-                    state = State.TWO;
-                } else
-                    throwMalformedNumberException(c);
+                stateOne(c);
                 break;
             case TWO:
                 return returnToken(TokenType.INTEGER_NUMBER, c);
             case THREE:
-                if (isNumber(c))
-                    state = State.FOUR;
-                else
-                    throwMalformedNumberException(c);
-                append(c);
+                stateThree(c);
                 break;
             case FOUR:
-                if (isNumber(c)) {
-                    append(c);
-                } else if (!isLetter(c)) {
-                    back();
-                    state = State.FIVE;
-                } else {
-                    throwMalformedNumberException(c);
-                }
+                stateFour(c);
                 break;
             case FIVE:
                 return returnToken(TokenType.REAL_NUMBER, c);
             case SIX:
-                if (isUnderline(c) || isLetter(c) || isNumber(c)) {
-                    append(c);
-                    break;
-                } else if (isOther(c)) {
-                    back();
-                    state = State.SEVEN;
-                } else {
-                    back();
-                    throw new MalformedTokenException("Malformed identifier - '" + (scanned + c) + "'");
-                }
+                stateSix(c);
                 break;
             case SEVEN:
-                if (isReservedWord(scanned))
-                    return returnToken(TokenType.RESERVED_WORD);
-                else
-                    return returnToken(TokenType.IDENTIFIER);
+                return stateSeven();
             case EIGHT:
-                append(c);
-                if (isEquals(c))
-                    state = State.TEN;
-                else
-                    state = State.NINE;
+                stateEight(c);
                 break;
             case NINE:
                 return returnToken(TokenType.ARITHMETIC_OPERATOR_ATTRIBUTION, c);
             case TEN:
                 return returnToken(TokenType.RELATIONAL_OPERATOR_EQUAL, c);
             case ELEVEN:
-                if (isNonConsumable(c) || isChar(c) || isUnderline(c) || isOpenParentesis(c))   //TODO adicionar o check para char aqui
-                    c = previousChar();
-
-                if (isMinus(c))
-                    type = TokenType.ARITHMETIC_OPERATOR_SUBTRACTION;
-                else if (isPlus(c))
-                    type = TokenType.ARITHMETIC_OPERATOR_SUM;
-                else if (isMult(c))
-                    type = TokenType.ARITHMETIC_OPERATOR_MULTIPLICATION;
-                else if (isDiv(c))
-                    type = TokenType.ARITHMETIC_OPERATOR_DIVISION;
-                else
-                    throw new UnrecognizedTokenException("Unrecognized token - '" + (scanned + c) + "'");
-
-                next();
-                next();
-                return returnToken(type, c);
+                return stateEleven(c);
             case TWELVE:
-                if (isComma(c))
-                    type = TokenType.SPECIAL_CHARACTER_COMMA;
-                else if (isSemicolon(c))
-                    type = TokenType.SPECIAL_CHARACTER_SEMICOLON;
-                else if (isOpenParentesis(c))
-                    type = TokenType.SPECIAL_CHARACTER_OPEN_PARENTHESIS;
-                else if (isCloseParentesis(c))
-                    type = TokenType.SPECIAL_CHARACTER_CLOSE_PARENTHESIS;
-                else if (isOpenCurlyBracket(c))
-                    type = TokenType.SPECIAL_CHARACTER_OPEN_CURLY_BRACKET;
-                else if (isCloseCurlyBracket(c))
-                    type = TokenType.SPECIAL_CHARACTER_CLOSE_CURLY_BRACKET;
-                else
-                    throw new UnrecognizedTokenException("Unrecognized token - '" + (scanned + c) + "'");
-
-                next();
-                return returnToken(type, c);
+                return stateTwelve(c);
             case THIRTEEN:
-                append(c);
-
-                if (!isRelationalOperator(c))
-                    c = previousChar();
-
-                if (isLessThan(c))
-                    state = State.FOURTEEN;
-                else if (isGreaterThan(c))
-                    state = State.SEVENTEEN;
-                else if (isDiff(c))
-                    state = State.TWENTY;
-                else
-                    throw new UnrecognizedTokenException("Unrecognized token - '" + (scanned + c) + "'");
-
+                stateThirteen(c);
                 break;
             case FOURTEEN:
-                c = nextChar();
-                if (isEquals(c))
-                    state = State.FIFTHTEEN;
-                else
-                    state = State.SIXTEEN;
+                stateFourteen();
                 break;
             case FIFTHTEEN:
                 return returnToken(TokenType.RELATIONAL_OPERATOR_LESS_THAN_OR_EQUAL_TO, c);
             case SIXTEEN:
                 return returnToken(TokenType.RELATIONAL_OPERATOR_LESS_THAN, c);
             case SEVENTEEN:
-                c = nextChar();
-                if (isEquals(c))
-                    state = State.EIGHTEEN;
-                else
-                    state = State.NINETEEN;
+                stateSeventeen();
                 break;
             case EIGHTEEN:
                 return returnToken(TokenType.RELATIONAL_OPERATOR_GREATER_THAN_OR_EQUAL_TO, c);
             case NINETEEN:
                 return returnToken(TokenType.RELATIONAL_OPERATOR_GREATER_THAN, c);
             case TWENTY:
-                append(c);
-                c = nextChar();
-                if (isEquals(c))
-                    state = State.TWENTY_ONE;
-                else
-                    throw new UnrecognizedTokenException("Unrecognized operator - '" + (scanned + c) + "'");
+                stateTwenty(c);
                 break;
             case TWENTY_ONE:
                 return returnToken(TokenType.RELATIONAL_OPERATOR_DIFFERENT, c);
             case TWENTY_TWO:
-                scanned = String.valueOf(c);    //Para remover o (') que foi appendido
-                if (isChar(c))
-                    state = State.TWENTY_THREE;
-                else
-                    throw new MalformedTokenException("Malformed char - (" + scanned + ")");
+                stateTwentyTwo(c);
                 break;
             case TWENTY_THREE:
-                if (isSingleQuotes(c))
-                    state = State.TWENTY_FOUR;
-                else
-                    throw new MalformedTokenException("Malformed char - (" + (scanned + c) + ")");
+                stateTwentyThree(c);
                 break;
             case TWENTY_FOUR:
                 return returnToken(TokenType.CHAR);
@@ -287,6 +165,191 @@ public class LexicalScanner {   //TODO adicionar linha (/r) e coluna (cada carac
                 throw new IllegalStateException("Estado inválido: " + state);
         }
         return null;
+    }
+
+    private void stateTwentyThree(char c) {
+        if (isSingleQuotes(c))
+            state = State.TWENTY_FOUR;
+        else
+            throw new MalformedTokenException("Malformed char - (" + (scanned + c) + ")");
+    }
+
+    private void stateTwentyTwo(char c) {
+        scanned = String.valueOf(c);    //Para remover o (') que foi appendido
+        if (isChar(c))
+            state = State.TWENTY_THREE;
+        else
+            throw new MalformedTokenException("Malformed char - (" + scanned + ")");
+    }
+
+    private void stateTwenty(char c) {
+        append(c);
+        c = nextChar();
+        if (isEquals(c))
+            state = State.TWENTY_ONE;
+        else
+            throw new UnrecognizedTokenException("Unrecognized operator - '" + (scanned + c) + "'");
+    }
+
+    private void stateSeventeen() {
+        char c;
+        c = nextChar();
+        if (isEquals(c))
+            state = State.EIGHTEEN;
+        else
+            state = State.NINETEEN;
+    }
+
+    private void stateFourteen() {
+        char c;
+        c = nextChar();
+        if (isEquals(c))
+            state = State.FIFTHTEEN;
+        else
+            state = State.SIXTEEN;
+    }
+
+    private void stateThirteen(char c) {
+        append(c);
+
+        if (!isRelationalOperator(c))
+            c = previousChar();
+
+        if (isLessThan(c))
+            state = State.FOURTEEN;
+        else if (isGreaterThan(c))
+            state = State.SEVENTEEN;
+        else if (isDiff(c))
+            state = State.TWENTY;
+        else
+            throw new UnrecognizedTokenException("Unrecognized token - '" + (scanned + c) + "'");
+    }
+
+    private Token stateTwelve(char c) {
+        TokenType type;
+        if (isComma(c))
+            type = TokenType.SPECIAL_CHARACTER_COMMA;
+        else if (isSemicolon(c))
+            type = TokenType.SPECIAL_CHARACTER_SEMICOLON;
+        else if (isOpenParentesis(c))
+            type = TokenType.SPECIAL_CHARACTER_OPEN_PARENTHESIS;
+        else if (isCloseParentesis(c))
+            type = TokenType.SPECIAL_CHARACTER_CLOSE_PARENTHESIS;
+        else if (isOpenCurlyBracket(c))
+            type = TokenType.SPECIAL_CHARACTER_OPEN_CURLY_BRACKET;
+        else if (isCloseCurlyBracket(c))
+            type = TokenType.SPECIAL_CHARACTER_CLOSE_CURLY_BRACKET;
+        else
+            throw new UnrecognizedTokenException("Unrecognized token - '" + (scanned + c) + "'");
+
+        next();
+        return returnToken(type, c);
+    }
+
+    private Token stateEleven(char c) {
+        TokenType type;
+        if (isNonConsumable(c) || isChar(c) || isUnderline(c) || isOpenParentesis(c))   //TODO adicionar o check para char aqui
+            c = previousChar();
+
+        if (isMinus(c))
+            type = TokenType.ARITHMETIC_OPERATOR_SUBTRACTION;
+        else if (isPlus(c))
+            type = TokenType.ARITHMETIC_OPERATOR_SUM;
+        else if (isMult(c))
+            type = TokenType.ARITHMETIC_OPERATOR_MULTIPLICATION;
+        else if (isDiv(c))
+            type = TokenType.ARITHMETIC_OPERATOR_DIVISION;
+        else
+            throw new UnrecognizedTokenException("Unrecognized token - '" + (scanned + c) + "'");
+
+        next();
+        next();
+        return returnToken(type, c);
+    }
+
+    private void stateEight(char c) {
+        append(c);
+        if (isEquals(c))
+            state = State.TEN;
+        else
+            state = State.NINE;
+    }
+
+    private Token stateSeven() {
+        if (isReservedWord(scanned))
+            return returnToken(TokenType.RESERVED_WORD);
+        else
+            return returnToken(TokenType.IDENTIFIER);
+    }
+
+    private void stateSix(char c) {
+        if (isUnderline(c) || isLetter(c) || isNumber(c)) {
+            append(c);
+            return;
+        } else if (isOther(c)) {
+            back();
+            state = State.SEVEN;
+        } else {
+            back();
+            throw new MalformedTokenException("Malformed identifier - '" + (scanned + c) + "'");
+        }
+    }
+
+    private void stateFour(char c) {
+        if (isNumber(c)) {
+            append(c);
+        } else if (!isLetter(c)) {
+            back();
+            state = State.FIVE;
+        } else {
+            throwMalformedNumberException(c);
+        }
+    }
+
+    private void stateThree(char c) {
+        if (isNumber(c))
+            state = State.FOUR;
+        else
+            throwMalformedNumberException(c);
+        append(c);
+    }
+
+    private void stateOne(char c) {
+        if (isNumber(c)) {
+            append(c);
+            return;
+        } else if (isDot(c)) {
+            append(c);
+            state = State.THREE;
+        } else if (isNonConsumable(c) || !isLetter(c)) {
+            back();
+            state = State.TWO;
+        } else
+            throwMalformedNumberException(c);
+    }
+
+    private void stateZero(char c) {
+        if (isNonConsumable(c))
+            return;
+
+        append(c);
+        if (isNumber(c))
+            state = State.ONE;
+        else if (isUnderline(c) || isLetter(c))
+            state = State.SIX;
+        else if (isEquals(c))
+            state = State.EIGHT;
+        else if (isAritmeticOperator(c))
+            state = State.ELEVEN;
+        else if (isSpecialChar(c)) {
+            back();
+            state = State.TWELVE;
+        } else if (isRelationalOperator(c))
+            state = State.THIRTEEN;
+        else if (isSingleQuotes(c))
+            state = State.TWENTY_TWO;
+        else
+            throw new UnrecognizedTokenException("Unrecognized symbol - '" + (scanned + c) + "'");
     }
 
     private void throwMalformedNumberException(char c) {
